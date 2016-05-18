739b1f757e416110406a962a1f334a3e98b979fb
Aleksandr Didenko <adidenko@mirantis.com>
2016-05-18 10:48:10 +0200

    Added YAML file parameter

--- deployment_scripts/puppet/manifests/create_custom_resources.pp
+++ deployment_scripts/puppet/manifests/create_custom_resources.pp
@@ -4,25 +4,37 @@ define myresource (
   $node_roles,
   $resource = $name
 ){
-
-  $roles_match = inline_template('<%
-    result = ""
-    @resource["node_roles"].each {|role|
-      result << @node_roles.find { |e| e =~ /#{role}/ }.to_s
-    }
-    %><%= result %>')
-
   if is_hash($resource) and has_key($resource, 'node_roles') and
-     size($roles_match)>0 {
-    create_resources($resource['type'], $resource['hash'])
+    has_key($resource, 'hash') and has_key($resource, 'type'){
+
+    $roles_match = inline_template('<%
+      result = ""
+      @resource["node_roles"].each {|role|
+        result << @node_roles.find { |e| e =~ /#{role}/ }.to_s
+      }
+      %><%= result %>')
+
+    if size($roles_match)>0 {
+      create_resources($resource['type'], $resource['hash'])
+    }
+  } else {
+    warning("Improperly defined resource: $resource")
   }
 }
 
+# Main
 $custom_resources = hiera('custom_resources', {})
 
+# Compile YAML textarea input if any
 if has_key($custom_resources, 'yaml'){
-  $additional_resources = parseyaml($custom_resources['yaml'])
-  myresources{$additional_resources:
+  myresources{parseyaml($custom_resources['yaml']):
+    node_roles => hiera('roles'),
+  }
+}
+
+# Compile YAML file input if any
+if has_key($custom_resources, 'yaml_data'){
+  myesources{parseyaml($custom_resources['yaml_data']):
     node_roles => hiera('roles'),
   }
 }
