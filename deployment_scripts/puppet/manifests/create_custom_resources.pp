7914c70b9c22e118f2ab2b310eeaca430cc21c37
Aleksandr Didenko <adidenko@mirantis.com>
2016-05-18 09:31:17 +0200

    Fixes, refactoring end yaml example

--- deployment_scripts/puppet/manifests/create_custom_resources.pp
+++ deployment_scripts/puppet/manifests/create_custom_resources.pp
@@ -1,11 +1,28 @@
 notice("MODULAR: create_custom_resources.pp")
-define myresources {
-  if is_hash($name) and size($name)>0{
-    create_resources($name['type'], $name['hash'])
+
+define myresource (
+  $node_roles,
+  $resource = $name
+){
+
+  $roles_match = inline_template('<%
+    result = ""
+    @resource["node_roles"].each {|role|
+      result << @node_roles.find { |e| e =~ /#{role}/ }.to_s
+    }
+    %><%= result %>')
+
+  if is_hash($resource) and has_key($resource, 'node_roles') and
+     size($roles_match)>0 {
+    create_resources($resource['type'], $resource['hash'])
   }
 }
 
-$custom_resources = hiera('custom_resources', {'yaml'=>[]})
-$additional_resources = $custom_resources['yaml']
-myresources{$additional_resources:}
+$custom_resources = hiera('custom_resources', {})
 
+if has_key($custom_resources, 'yaml'){
+  $additional_resources = parseyaml($custom_resources['yaml'])
+  myresources{$additional_resources:
+    node_roles => hiera('roles'),
+  }
+}
